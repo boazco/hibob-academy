65e22b7372d24c0ba3821bc4be1451802b06e84f
Boaz <boaz.cohen@hibob.io>
2024-09-16 17:22:24 +0300

    saving

--- src/main/kotlin/com/hibob/academy/resource/ownerResource.kt
+++ src/main/kotlin/com/hibob/academy/resource/ownerResource.kt
@@ -4,6 +4,16 @@ import com.hibob.academy.types.Owner
 import jakarta.ws.rs.*
 import jakarta.ws.rs.core.Response
 import org.springframework.stereotype.Controller
+
+
+
+
+
+
+
+
+
+
 import jakarta.ws.rs.core.MediaType
 import org.springframework.web.bind.annotation.RequestBody
 
