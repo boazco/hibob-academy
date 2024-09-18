7b776d60862630e4ef3e065e1d7c2e070bd6009b
Boaz <boaz.cohen@hibob.io>
2024-09-18 17:08:24 +0300

    fix test

--- src/test/kotlin/com/hibob/academy/dao/PetDaoTest.kt
+++ src/test/kotlin/com/hibob/academy/dao/PetDaoTest.kt
@@ -251,10 +251,10 @@ class PetDaoTest @Autowired constructor(private val sql: DSLContext) {
 
     @Test
     fun `count by type`() {
-        petDao.createPet("Jerry", "Dog", companyId, Date.valueOf(LocalDate.now()), null)
-        petDao.createPet("Johans", "Dog", companyId, Date.valueOf(LocalDate.now()), null)
-        petDao.createPet("jj", "Cat", companyId, Date.valueOf(LocalDate.now()), null)
-        petDao.createPet("hh", "Cat", companyId, Date.valueOf(LocalDate.now()), null)
+        petDao.createPet(PetNoId("Jerry", "Dog", companyId, Date.valueOf(LocalDate.now()), null))
+        petDao.createPet(PetNoId("Johans", "Dog", companyId, Date.valueOf(LocalDate.now()), null))
+        petDao.createPet(PetNoId("jj", "Cat", companyId, Date.valueOf(LocalDate.now()), null))
+        petDao.createPet(PetNoId("hh", "Cat", companyId, Date.valueOf(LocalDate.now()), null))
         assertEquals(mapOf("Dog" to 2, "Cat" to 2), petDao.countPetsByType(companyId))
     }
 
