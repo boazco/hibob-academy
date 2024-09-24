8973ee826ca7930a7c53146746a49c37d4858b6d
Boaz <boaz.cohen@hibob.io>
2024-09-24 10:51:03 +0300

    saving

--- src/main/kotlin/com/hibob/feedback/resource/FeedbackResource.kt
+++ src/main/kotlin/com/hibob/feedback/resource/FeedbackResource.kt
@@ -42,7 +42,7 @@ class FeedbackResource(private val feedbackService: FeedbackService) {
 
         throwIfNotAuthorized(activeUser)
         val feedback = feedbackService.getFeedback(feedbackId, activeUser)
-        return Response.ok(feedback).build() //TO DO CHANGE IT to return the output from the service.
+        return Response.ok(feedback).build()
     }
 
     private fun throwIfNotAuthorized(activeUser: ActiveUser) {
