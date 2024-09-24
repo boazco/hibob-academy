60ab3c422d93ed5bf3f97d2f66e4a83650a22b44
Boaz <boaz.cohen@hibob.io>
2024-09-24 09:52:51 +0300

    saving

--- src/main/kotlin/com/hibob/feedback/service/FeedbackService.kt
+++ src/main/kotlin/com/hibob/feedback/service/FeedbackService.kt
@@ -3,7 +3,11 @@ package com.hibob.feedback.service
 import com.hibob.feedback.dao.ActiveUser
 import com.hibob.feedback.dao.Feedback
 import com.hibob.feedback.dao.FeedbackInput
+import jakarta.ws.rs.BadRequestException
+import jakarta.ws.rs.NotAuthorizedException
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.server.ResponseStatusException
 import java.sql.Date
 import java.time.LocalDate
 import java.util.*
@@ -12,20 +16,27 @@ import java.util.*
 class FeedbackService(/* Will need to get the dao*/) {
 
     fun createFeedback(feedbackInput: FeedbackInput, activeUser: ActiveUser): UUID {
-        val feedbackId = UUID.randomUUID()
-        val feedback =
-            Feedback(feedbackId, Date.valueOf(LocalDate.now()), activeUser.companyId, feedbackInput.feedbackMessage)
-        //TO DO calling the Dao create function with the feedback with employeeId/null depend on the feedbackInput.isAnonymous
+        return if (feedbackInput.isAnonymous) {
+            FeedbackDao.createFeedback(feedbackInput, activeUser.copy(employeeId = null))
+        } else {
+            feedbackDao.createFeedback(feedbackInput, activeUser)
+        }
         //Will need to add department into the feedback in Phase2 (from the DB, here or in the filter)
-        //In dao the "status" property will always be the same in creation
-        return feedbackId
     }
 
     fun getFeedback(feedbackId: UUID, activeUser: ActiveUser): Feedback? {
-        val feedback = 0 //Will be return feedbackDao.getFeedback(args)
-        //TO DO: add fetching of active user department and role. if role != admin && department != HR && employeeId != feedBack employeeId
-        // Throw exception 401 Unauthorized
-        return null //Do we want to throw exception when no feedback is found? or only when no access
+        val feedback: Feedback = FeedbackDao.getFeedback(feedbackId, activeUser)
+        if (EmployeesDao.getDepartmentById(activeUser.employeeId) != "HR"
+            && EmployeesDao.getRoleById(activeUser.employeeId) != "Admin"
+            && EmployeeDao.getEmployeeIdByFeedbackId(feedbackId) != activeUser.employeeId) {
+            throw ResponseStatusException(HttpStatus.UNAUTHORIZED
+                , "Unauthorized Access- Trying to fetch feedback which is not yours, while youre not HR or admin")
+        }
+            val feedback = EmployeeDao.getFeedback(feedbackId, activeUser)
+        if(feedback == null) {
+            throw BadRequestException(HttpStatus.BAD_REQUEST, "Feedback not found")
+        }
+        return feedback //Do we want to throw exception when no feedback is found? or only when no access
     }
 
 }
