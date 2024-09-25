5ef15174c462387fcad4c5f89aef601c0cf443dd
Boaz <boaz.cohen@hibob.io>
2024-09-25 12:22:41 +0300

    add verifyEmployeeInDB

--- src/main/kotlin/com/hibob/academy/service/SessionService.kt
+++ src/main/kotlin/com/hibob/academy/service/SessionService.kt
@@ -4,6 +4,7 @@ import com.hibob.feedback.dao.ActiveUser
 import com.hibob.feedback.dao.EmployeesDao
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import jakarta.ws.rs.BadRequestException
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -17,8 +18,7 @@ class SessionService(private val employeesDao: EmployeesDao) {
     }
 
     fun createJwtToken(activeUser: ActiveUser): String {
-        //val employee = employeesDao.getEmployeeById(activeUser)
-        // employee ?: throw exception
+        verifyEmployeeInDB(activeUser)
         return Jwts.builder()
             .setHeaderParam("typ", "JWT")
             .claim("employeeId", activeUser.employeeId)
@@ -29,6 +29,14 @@ class SessionService(private val employeesDao: EmployeesDao) {
             .setExpiration(Date.from(LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC)))
             .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
             .compact()
+    }
+
+    private fun verifyEmployeeInDB(activeUser: ActiveUser) {
+        val employee =
+            employeesDao.getEmployeeByActiveUser(activeUser) //throws if no employee with this id in this company
+        if (employee.role != activeUser.role || employee.department != activeUser.department) {
+            throw BadRequestException("The employee info is not accurate (Role/Department)")
+        }
     }
 
 }
