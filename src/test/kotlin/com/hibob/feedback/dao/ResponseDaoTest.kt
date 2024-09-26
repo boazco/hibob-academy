d5b7a98cbe154b54a09b1d3b367f6cdfbf917c99
Boaz <boaz.cohen@hibob.io>
2024-09-26 11:35:15 +0300

    response API

--- src/test/kotlin/com/hibob/feedback/dao/ResponseDaoTest.kt
+++ src/test/kotlin/com/hibob/feedback/dao/ResponseDaoTest.kt
@@ -2,9 +2,30 @@ package com.hibob.feedback.dao
 
 import com.hibob.academy.utils.BobDbTest
 import org.jooq.DSLContext
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
 
 @BobDbTest
 class ResponseDaoTest @Autowired constructor(private val sql: DSLContext){
+    private val responseDao = ResponseDao(sql)
+    private val responseTable = ResponseDao.ResponseTable.instance
+    private val companyId = UUID.randomUUID()
+    private val hrId = UUID.randomUUID()
+    private val responseInput = ResponseInput(UUID.randomUUID(), "Response for testing", hrId)
+    private val activeUser = ActiveUser(hrId, companyId, Role.ADMIN, Department.HR)
+
+    @BeforeEach
+    @AfterEach
+    fun cleanup() {
+        sql.deleteFrom(responseTable).where(responseTable.companyId.eq(companyId)).execute()
+    }
+
+    @Test
+    fun `create response should return response UUID`(){
+        assertTrue(responseDao.createResponse(responseInput, activeUser) is UUID)
+    }
 }
---
+++ src/test/kotlin/com/hibob/feedback/service/ResponseServiceTest.kt
@@ -0,0 +1,32 @@
+package com.hibob.feedback.service
+
+import com.hibob.feedback.dao.*
+import jakarta.ws.rs.BadRequestException
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import java.util.*
+
+class ResponseServiceTest{
+    private val responseDaoMock = mock<ResponseDao>()
+    private val feedbackDaoMock = mock<FeedbackDao>()
+    private val service = ResponseService(responseDaoMock, feedbackDaoMock)
+    private val activeUser = ActiveUser(UUID.randomUUID(), UUID.randomUUID(), Role.ADMIN, Department.HR)
+    private val feedbackId = UUID.randomUUID()
+    private val responseInput = ResponseInput(feedbackId,"Feedback for testing", activeUser.employeeId)
+
+
+    @Test
+    fun `when no feedback is found, it throws exception`() {
+        whenever(feedbackDaoMock.getFeedback(responseInput.feedbackId, activeUser)).thenThrow(BadRequestException::class.java)
+        org.junit.jupiter.api.assertThrows<BadRequestException> { service.createResponse(responseInput, activeUser) }
+    }
+
+    @Test
+    fun `change status is being called on a valid response`(){
+        service.createResponse(responseInput, activeUser)
+        verify(feedbackDaoMock).changeStatus(responseInput.feedbackId, Status.REVIEWED, activeUser)
+    }
+
+}
